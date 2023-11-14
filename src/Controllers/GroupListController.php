ae732c33eb81e6da15edde38ed292bffb32944ea
Dominik Grothaus <dg@mc-kg.de>
2023-11-14 15:24:19 +0100

    Add members without dedicated group to page

--- src/Controllers/GroupListController.php
+++ src/Controllers/GroupListController.php
@@ -5,7 +5,9 @@ namespace ClarkWinkelmann\GroupList\Controllers;
 use ClarkWinkelmann\GroupList\GroupListItem;
 use ClarkWinkelmann\GroupList\Serializers\GroupListItemSerializer;
 use Flarum\Api\Controller\AbstractListController;
+use Flarum\Group\Group;
 use Flarum\Http\RequestUtil;
+use Flarum\User\User;
 use Psr\Http\Message\ServerRequestInterface;
 use Tobscure\JsonApi\Document;
 
@@ -23,11 +25,18 @@ class GroupListController extends AbstractListController
         RequestUtil::getActor($request)->assertCan('clarkwinkelmann-group-list.see');
 
         $items = GroupListItem::query()->orderBy('order')->get();
+        $uu = User::query()->doesntHave('groups')->orderBy('id', 'asc')->get();
 
         $items->load([
             'group',
             'members',
         ]);
+
+        // Add members without any dedicated group
+        $membersWithoutGroup = new GroupListItem();
+        $membersWithoutGroup->setRelation('group', Group::query()->find(Group::MEMBER_ID))
+            ->setRelation('members', $uu);
+        $items->add($membersWithoutGroup);
 
         return $items;
     }
